5a7028c05530063affb52390e08e93b1f78c657a
Shivam800750 <24121cn044@glbitm.ac.in>
2026-08-21 14:57:40 +0000

    datatypesummary

---
+++ .devcontainer/01_basics/datatypes_summary.js
@@ -0,0 +1,39 @@
+// primitive
+
+// 7 types : String, Number, Boolean, Null, undefined, Symbol, BigInt
+
+const score = 100
+const scorevalue = 100.3
+
+const isLogged = false
+const outsidetemp = null
+let userEmail; // undefined
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId);
+
+
+
+const bigNumber = 88977777336374n
+
+
+
+// Reference (Non primitive):-
+
+// Array, Objects, Functions
+
+const heros = ["Shaktiman", "naagraj", "doga"]
+let myObj = {
+    name: "shivam",
+    age: 20,
+}
+
+ const myFunction = function(){
+    console.log("Hello world");
+ }
+console.log(typeof score);
+console.log(typeof scorevalue);
+console.log(typeof anotherId);
+
